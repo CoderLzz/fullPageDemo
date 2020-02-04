591c6f05af5d421b561a1474e2e840b40c655e04
Mr.Li <1334713298@qq.com>
2020-02-04 14:56:56 +0800

    第一屏完成

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ $(function () {
     $('#container').fullpage({
         sectionsColor:["#fadd67", "#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
         verticalCentered:false,
-        navigation:true
+        navigation:true,
+        afterLoad:function(link,index){
+            $('.section').eq(index.index).addClass('active')
+        }
     });
 });
