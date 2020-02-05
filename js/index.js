0e7b0fef7bc78f8830d466dc4ce0116f3f4cc22e
Mr.Li <1334713298@qq.com>
2020-02-05 17:56:03 +0800

    第三屏和第二屏完成

--- js/index.js
+++ js/index.js
@@ -3,9 +3,20 @@ $(function () {
         sectionsColor:["#fadd67", "#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
         verticalCentered:false,
         navigation:true,
+        scrollingSpeed:1000,
         afterLoad:function(link,index){
-            console.log(index);
             $('.section').eq(index.index).addClass('lzz')
+        },
+        afterRender:function(){
+            $('.down').on('click',function(){
+                $.fn.fullpage.moveSectionDown()
+            })
+        },
+        onLeave:function(index,nextIndex){
+            if(index.index==1&&nextIndex.index==2){
+                $('.section').eq(index.index).addClass('oym')
+                $('.section').eq(nextIndex.index).css({zIndex:-1})
+            }
         }
-    });
+    }); 
 });
