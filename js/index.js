61c954ebc16c10f1fc60e7ac29c7e7ba448c1282
Mr.Li <1334713298@qq.com>
2020-02-08 19:27:24 +0800

    所有功能完成

--- js/index.js
+++ js/index.js
@@ -1,32 +1,36 @@
 $(function () {
     $('#container').fullpage({
-        sectionsColor:["#fadd67", "#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
-        verticalCentered:false,
-        navigation:true,
-        scrollingSpeed:1000,
-        afterLoad:function(link,index){
+        sectionsColor: ["#fadd67", "#84a2d4", "#ef674d", "#ffeedd", "#d04759", "#84d9ed", "#8ac060"],
+        verticalCentered: false,
+        navigation: true,
+        scrollingSpeed: 1000,
+        afterLoad: function (link, index) {
             $('.section').eq(index.index).addClass('lzz')
         },
-        afterRender:function(){
-            $('.down').on('click',function(){
+        afterRender: function () {
+            $('.down').on('click', function () {
                 $.fn.fullpage.moveSectionDown()
             })
+            var hand = document.querySelector('.page8 .hand')
+            document.onmousemove = function (e) {
+                hand.style.top = e.clientY+30 + 'px'
+                hand.style.left = e.clientX - 50 + 'px'
+            }
+            $('.page8 .again').on('click', function () {
+                $('.lzz,.oym').removeClass('lzz').removeClass('oym')
+                $.fn.fullpage.moveTo(1)
+            })
         },
-        onLeave:function(index,nextIndex){
-            if(index.index==1&&nextIndex.index==2){
+        onLeave: function (index, nextIndex) {
+            if (index.index == 1 && nextIndex.index == 2) {
                 $('.section').eq(index.index).addClass('oym')
-            }else if(index.index==2&&nextIndex.index==3){
+            } else if (index.index == 2 && nextIndex.index == 3) {
                 $('.section').eq(index.index).addClass('oym')
-            }else if(index.index==3&&nextIndex.index==4){
+            } else if (index.index == 3 && nextIndex.index == 4) {
                 $('.section').eq(index.index).addClass('oym')
-            }else if(index.index==4&&nextIndex.index==5){
+            } else if (index.index == 4 && nextIndex.index == 5) {
                 $('.section').eq(index.index).addClass('oym')
             }
         }
-    }); 
-    var hand=document.querySelector('.page8 .hand')
-    document.onmousemove=function(e){
-        hand.style.top=e.clientY+'px'
-        hand.style.left=e.clientX-50+'px'
-    }
+    });
 });
